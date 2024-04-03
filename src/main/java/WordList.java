1d6bd5d1fc85d1447654b713f020c88570a62d5a
lilahanders817 <143014915+lilahanders817@users.noreply.github.com>
2024-04-03 09:57:58 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
-public class WordList
-{
+public class WordList{
   private ArrayList <String> myList;
 
   public WordList() { myList = new ArrayList<String>(); }
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-    /* to be implemented in part (a) */
-    
+  public int numWordsOfLength(int len){
+    int count = 0;
+    for(int i = 0; i<myList.size(); i++){
+      if(myList.get(i).length() == len){
+        count++;
+      }
+    }
+   return count; 
   }
 
-  public void removeWordsOfLength(int len)
-  {
+  public void removeWordsOfLength(int len){
     /* to be implemented in part (b) */
 
   }
